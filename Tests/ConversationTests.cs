91c480a9bc21cc4c12d46b53aa230e398703556a
Isaiahalfred <156960946+Isaiahalfred@users.noreply.github.com>
2026-04-27 15:25:37 -0400

    Add unit tests code for ChatService methods

--- Tests/ConversationTests.cs
+++ Tests/ConversationTests.cs
@@ -1,1 +1,30 @@
+using Xunit;
 
+public class ConversationTests
+{
+    [Fact]
+    public void AddMessage_ShouldNotFail()
+    {
+        var service = new ChatService();
+
+        var response = service.SendMessage("Hello");
+
+        Assert.NotNull(response);
+    }
+
+    [Fact]
+    public void SaveLoad_ShouldWork()
+    {
+        var service = new ChatService();
+
+        service.SendMessage("Test message");
+        service.Save("test.json");
+
+        var newService = new ChatService();
+        newService.Load("test.json");
+
+        var response = newService.SendMessage("Hi again");
+
+        Assert.NotNull(response);
+    }
+}
